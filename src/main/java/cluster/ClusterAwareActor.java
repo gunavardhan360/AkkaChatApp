14ffb34b200ad3387aec0bb258f84e171eb7da41
lordguna <55921156+lordguna@users.noreply.github.com>
2022-11-29 13:09:59 +0530

    entire application

--- src/main/java/cluster/ClusterAwareActor.java
+++ src/main/java/cluster/ClusterAwareActor.java
@@ -1,64 +1,40 @@
 package cluster;
 
-import akka.actor.AbstractLoggingActor;
-import akka.actor.ActorSelection;
-import akka.actor.Cancellable;
-import akka.actor.Props;
+import akka.actor.*;
 import akka.cluster.Cluster;
 import akka.cluster.Member;
 import akka.cluster.MemberStatus;
+import akka.pattern.Patterns;
+import akka.util.Timeout;
+import scala.concurrent.Await;
+import scala.concurrent.Future;
+import scala.concurrent.duration.Duration;
 
-import java.io.Serializable;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
+import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicInteger;
 
 class ClusterAwareActor extends AbstractLoggingActor {
     private final Cluster cluster = Cluster.get(context().system());
-//    private final FiniteDuration tickInterval = Duration.create(10, TimeUnit.SECONDS);
-    private Cancellable ticker;
+    private final Timeout timeout = new Timeout(Duration.create(3, "seconds"));
+    public static chatApplication.userName user = new chatApplication.userName();
 
-    public static String userName;
+    public ClusterAwareActor(String name) { this.user.setUserName(name); }
 
     @Override
     public Receive createReceive() {
         return receiveBuilder()
-                .matchEquals("getUserName", s -> getUserName())
-                .matchEquals("onlineUserList", s -> onlineUserList())
-                .match(String.class, s -> s.startsWith("@@"), this::handleMessage)
-                .match(String.class, s -> s.startsWith("@"), this::sendMessage)
-                .match(chatApplication.userName.class, this::displayName)
-                .matchEquals("stop", m -> getContext().stop(getSelf()))
+                .match(chatApplication.userName.class, s -> getSender().tell(this.user, self()))
+                .match(chatApplication.onlineUserList.class, s -> onlineUserList())
+                .match(chatApplication.messageAll.class, this::sendMessage)
+                .match(chatApplication.messageFew.class, this::sendMessage)
+                .match(chatApplication.messageTo.class, this::sendMessage)
+                .match(chatApplication.messageFrom.class, this::handleMessage)
+                .matchAny(this::printError)
                 .build();
-    }
-
-    private void handleMessage(String msg){
-        List<String> words = List.of(msg.split(" "));
-        String Message = String.join(" ", words.subList(2, words.size()));
-        if(userName.equals(words.get(1).substring(1))){
-            System.out.println(words.get(0).substring(1) + " -> " + Message);
-        }
-    }
-
-    private void sendMessage(String msg){
-        Member me = cluster.selfMember();
-        log().info("I {} am querying for list", me);
-
-        cluster.state().getMembers().forEach(member -> {
-            if (!me.equals(member) && member.status().equals(MemberStatus.up())) {
-                String path = member.address().toString() + self().path().toStringWithoutAddress();
-                ActorSelection actorSelection = context().actorSelection(path);
-                actorSelection.tell("@@" + userName + " " + msg, self());
-            }
-        });
-    }
-
-    private void displayName(chatApplication.userName user) {
-        System.out.println("@" + user.userName);
-    }
-
-    private void getUserName() {
-        chatApplication.userName name = new chatApplication.userName(userName);
-        sender().tell(name, self());
     }
 
     private void onlineUserList() {
@@ -69,77 +45,127 @@ class ClusterAwareActor extends AbstractLoggingActor {
         cluster.state().getMembers().forEach(member -> {
             if (!me.equals(member) && member.status().equals(MemberStatus.up())) {
                 String path = member.address().toString() + self().path().toStringWithoutAddress();
+                System.out.println(path);
                 ActorSelection actorSelection = context().actorSelection(path);
-                actorSelection.tell("getUserName", self());
+                System.out.println("@" + getUserName(actorSelection));
                 count.getAndIncrement();
             }
         });
         if(count.get() <= 1) System.out.println("No Online Users are present right now!!!");
     }
 
+    private void sendMessage(chatApplication.messageAll msgAll){
+        List<ActorSelection> actorSelections = getActorWithUserName(null);
+        sendViaChild(actorSelections, new chatApplication.messageFrom(this.user.getFirstName(), msgAll.getMessage()));
+
+    }
+    private void sendMessage(chatApplication.messageFew msgFew){
+        List<ActorSelection> actorSelections = getActorWithUserName(msgFew.getUserNames());
+        sendViaChild(actorSelections, new chatApplication.messageFrom(this.user.getFirstName(), msgFew.getMessage()));
+    }
+
+    private void sendViaChild(List<ActorSelection> actorSelectionList, chatApplication.messageFrom msgFrom){
+        for(ActorSelection actorSelection: actorSelectionList){
+            ActorRef childActor = context().actorOf(ChildClusterAwareActor.props(actorSelection, this.user.getFirstName()));
+            childActor.tell(msgFrom, self());
+        }
+    }
+
+    private void sendMessage(chatApplication.messageTo msgTo){
+        ActorSelection actorSelection = getActorWithUserName(List.of(new String[]{msgTo.getMessageTo()})).get(0);
+        if(actorSelection != null){
+            actorSelection.tell(new chatApplication.messageFrom(this.user.getFirstName(), msgTo.getMessage()), self());
+        } else {
+            System.out.println("No user found with First Name as " + msgTo.getMessageTo() + "\n-> ");
+        }
+    }
+
+    private List<ActorSelection> getActorWithUserName(List<String> userNames) {
+        Member me = cluster.selfMember();
+        log().info("I {} am querying for list", me);
+        List<ActorSelection> actorSelections = null;
+        for(Member member: cluster.state().getMembers()){
+            if (!me.equals(member) && member.status().equals(MemberStatus.up()) && !member.address().toString().contains("2551")) {
+                String path = member.address().toString() + self().path().toStringWithoutAddress();
+                ActorSelection temp = context().actorSelection(path);
+                String tempName = getUserName(temp).split(" ")[0];
+                if(userNames == null | userNames.contains(tempName))
+                    actorSelections.add(temp);
+            }
+        }
+        return actorSelections;
+    }
+
+    private String getUserName(ActorSelection actorSelection){
+        chatApplication.userName response = new chatApplication.userName();
+        Future<Object> f = Patterns.ask(actorSelection, new chatApplication.userName(), timeout);
+        try {
+            response = (chatApplication.userName) Await.result(f, timeout.duration());
+        } catch (TimeoutException | InterruptedException e){
+            e.printStackTrace();
+        }
+        if(response.getLastName() == null) return response.getFirstName();
+        return response.getFirstName() + " " + response.getLastName();
+    }
+
+    private void handleMessage(chatApplication.messageFrom msgFrom){
+        System.out.println("Message From User " + msgFrom.getMessageFrom() + " -> " + msgFrom.getMessage() + "\n-> ");
+    }
+
+    private void printError(Object o) { System.out.println("Not a valid Functionality"); }
+
     @Override
     public void preStart() {
         log().debug("Start");
-//        ticker = context().system().scheduler()
-//                .schedule(Duration.Zero(),
-//                        tickInterval,
-//                        self(),
-//                        "tick",
-//                        context().system().dispatcher(),
-//                        null);
     }
 
     @Override
     public void postStop() {
-        ticker.cancel();
         log().info("Stop");
     }
 
     static Props props(String name) {
-        userName = name;
-        return Props.create(ClusterAwareActor.class);
+        return Props.create(ClusterAwareActor.class, () -> new ClusterAwareActor(name));
+    }
+}
+
+
+class ChildClusterAwareActor extends AbstractLoggingActor {
+    public static chatApplication.userName user = new chatApplication.userName();
+    public ActorSelection actorSelection;
+
+    public ChildClusterAwareActor(ActorSelection actorSelection, String name) {
+        this.user.setUserName(name);
+        this.actorSelection = actorSelection;
     }
 
-    interface chatApplication{
-        class userName implements Serializable{
-            final String userName;
-
-            userName(String name) {
-                userName = name;
-            }
-        }
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+                .match(chatApplication.messageFrom.class, this::handleMessage)
+                .match(chatApplication.stopActor.class, s -> getContext().stop(getSelf()))
+                .matchAny(this::printError)
+                .build();
     }
-//
-//    interface Message {
-//        class Ping implements Serializable {
-//            final long time;
-//
-//            Ping() {
-//                time = System.nanoTime();
-//            }
-//
-//            @Override
-//            public String toString() {
-//                return String.format("%s[%dus]", getClass().getSimpleName(), time);
-//            }
-//        }
-//
-//        class Pong implements Serializable {
-//            final long pingTime;
-//
-//            private Pong(long pingTime) {
-//                this.pingTime = pingTime;
-//            }
-//
-//            static Pong from(Ping ping) {
-//                return new Pong(ping.time);
-//            }
-//
-//            @Override
-//            public String toString() {
-//                final double elapsed = (System.nanoTime() - pingTime) / 1000000000.0;
-//                return String.format("%s[elapsed %.9fs, %dus]", getClass().getSimpleName(), elapsed, pingTime);
-//            }
-//        }
-//    }
+
+    private void handleMessage(chatApplication.messageFrom msgFrom) {
+        this.actorSelection.tell(msgFrom, ActorRef.noSender());
+        getSelf().tell(new chatApplication.stopActor(), ActorRef.noSender());
+    }
+
+    private void printError(Object o) { System.out.println("Not a valid Functionality"); }
+
+    @Override
+    public void preStart() {
+        log().debug("Start");
+    }
+
+    @Override
+    public void postStop() {
+        log().info("Stop");
+    }
+
+    static Props props(ActorSelection actorSelection, String name) {
+        return Props.create(ChildClusterAwareActor.class, () -> new ChildClusterAwareActor(actorSelection, name));
+    }
 }
